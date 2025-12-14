f7cf7627d79ad5f15a4da43b5b44d2023b68a173
vboxuser <jhond999@gmail.com>
2025-12-14 19:18:35 -0300

    update entrega final

--- conftest.py
+++ conftest.py
@@ -3,6 +3,16 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import logging
 
+import os
+import pytest
+
+# Verificar si la carpeta "reports" existe, y si no, crearla
+if not os.path.exists('reports'):
+    os.makedirs('reports')
+
+# Ejecutar pytest programáticamente
+os.system('pytest --html=reports/report.html --maxfail=1 --disable-warnings')
+
 def pytest_configure():
     logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 
